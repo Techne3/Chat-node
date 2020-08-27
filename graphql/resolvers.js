01a8df947d2f363a6520e59034af122f83f6152c
Timothy Sharkey <Techne3@gmail.com>
2020-08-27 15:30:47 -0600

    Added validation for login data Hashed password with bcrypt

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const bcrypt = require("bcryptjs");
+const { UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
@@ -15,21 +16,49 @@ module.exports = {
   Mutation: {
     register: async (_, args) => {
       let { username, email, password, confirmPassword } = args;
+      let errors = {};
       try {
-        // TODO: validate input data
-        // TODO: Check if username / email exist
-        // TODO: Hash pass
+        // validate input data
+        if (email.trim() === "") errors.email = "email must not be empty";
+        if (username.trim() === "")
+          errors.username = "username must not be empty";
+        if (password.trim() === "")
+          errors.password = "password must not be empty";
+        if (confirmPassword.trim() === "")
+          errors.confirmPassword = "repeat password must not be empty";
+
+        if (password !== confirmPassword)
+          errors.confirmPassword = "passwords must match";
+
+        // // Check if username / email exist
+        // const userByUsername = await User.findOne({ where: { username } });
+        // const userByEmail = await User.findOne({ where: { email } });
+
+        // if (userByUsername) errors.username = "Username is taken";
+        // if (userByEmail) errors.email = "Email is taken";
+
+        if (Object.keys(errors).length > 0) {
+          throw errors;
+        }
+        // hash password
         password = await bcrypt.hash(password, 6);
-        // TODO: Create user
+        // crate user
         const user = await User.create({
           username: username,
           email: email,
           password: password,
         });
-        // TODO: Return User
         return user;
       } catch (err) {
-        throw err;
+        console.log(err);
+        if (err.name === "SequelizeUniqueConstraintError") {
+          err.errors.forEach(
+            (e) => (errors[e.path] = `${e.path} is already taken`)
+          );
+        } else if (err.name === "SequelizeValidationError") {
+          err.errors.forEach((e) => (errors[e.path] = e.message));
+        }
+        throw new UserInputError("Bad Input", { errors });
       }
     },
   },
